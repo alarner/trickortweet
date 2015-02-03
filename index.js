867482cbb583d7cbfa0ae8b92ade6295c0c2f0bd
Aaron Larner <anlarner@gmail.com>
2015-02-03 17:06:07 -0600

    Twitter streaming service implementation

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var myArray = [1,2,3];
-myArray.push(4);
-console.log(myArray);
+var twitterCredentials = require('./config/twitter');
+
+var Twitter = require('node-tweet-stream');
+
+var t = new Twitter(twitterCredentials);
+
+t.on('tweet', function (tweet) {
+	console.log(tweet.text+' - '+tweet.user.screen_name);
+});
+
+t.on('error', function (err) {
+  console.log('Oh no')
+});
+
+t.track('#tiynode');
+// var Twitter = require('twitter');
+
+
+// var client = new Twitter(twitterCredentials);
+
+// var params = {q: '"#tiynode"'};
+// client.get('search/tweets', params, function(error, tweets, response){
+// 	if (!error) {
+// 		for(var i=0; i<tweets.statuses.length; i++) {
+// 			console.log(tweets.statuses[i].text);
+// 		}
+// 	}
+// 	else {
+// 		console.log(error);
+// 	}
+// });
